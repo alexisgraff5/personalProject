7d424b23df6a85ea10b1b1970cc8f5d94e72f29b
alexisgraff5 <lex.graff@gmail.com>
2016-12-05 10:39:29 -0700

    continued to create views for each product

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,25 @@ angular.module('nixonClone', ['ui.router']).config(function($urlRouterProvider, 
     controller: 'mainCtrl'
   })
     .state('comp', {
-     url: '/comp',
+     url: '/mens-comp',
      templateUrl: './views/mens/comp/compTemp.html',
      controller: 'productCtrl'
-});
+   })
+    .state('corporal', {
+    url: '/mens-corporal',
+    templateUrl: './views/mens/corporal/corporalTemp.html',
+    controller: 'productCtrl'
+  })
+    .state('rollo', {
+    url: '/mens-rollo',
+    templateUrl: './views/mens/rollo/rolloTemp.html',
+    controller: 'productCtrl'
+  })
+    .state('kenzi', {
+    url: '/womens-kenzi',
+    templateUrl: './views/womens/kenzi/kenziTemp.html',
+    controller: 'productCtrl'
+  })
 
   $urlRouterProvider.otherwise('/home');
 
