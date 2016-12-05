1c3b755ace0f96bbd6061f9842ecd8c1d199bfdf
alexisgraff5 <lex.graff@gmail.com>
2016-12-05 16:12:31 -0700

    created views for search by band material

--- public/js/app.js
+++ public/js/app.js
@@ -60,6 +60,51 @@ angular.module('nixonClone', ['ui.router']).config(function($urlRouterProvider, 
     templateUrl: './views/womens/gi/giSsTemp.html',
     controller: 'productCtrl'
   })
+  .state('cart', {
+    url: '/cart',
+    templateUrl: './views/cart.html',
+    controller: 'productCtrl'
+  })
+  .state('womens-bands', {
+    url: '/womens-band-material',
+    templateUrl: './views/womens/bands/womensBandMaterial.html',
+    controller: 'productCtrl'
+  })
+  .state('womens-bands-stainless', {
+    url: '/womens-material-stainless',
+    templateUrl: './views/womens/bands/womensStainlessSearch.html',
+    controller: 'productCtrl'
+  })
+  .state('womens-bands-leather', {
+    url: '/womens-material-leather',
+    templateUrl: './views/womens/bands/womensLeatherSearch.html',
+    controller: 'productCtrl'
+  })
+  .state('womens-bands-nylon', {
+    url: '/womens-material-nylon',
+    templateUrl: './views/womens/bands/womensNylonSearch.html',
+    controller: 'productCtrl'
+  })
+  .state('mens-bands-material', {
+    url: '/mens-band-material',
+    templateUrl: './views/mens/bands/mensBandMaterial.html',
+    controller: 'productCtrl'
+  })
+  .state('mens-bands-stainless', {
+    url: '/mens-material-stainless',
+    templateUrl: './views/mens/bands/mensStainlessSearch.html',
+    controller: 'productCtrl'
+  })
+  .state('mens-bands-leather', {
+    url: '/mens-material-leather',
+    templateUrl: './views/mens/bands/mensLeatherSearch.html',
+    controller: 'productCtrl'
+  })
+  .state('mens-bands-silicone', {
+    url: '/mens-material-silicone',
+    templateUrl: './views/mens/bands/mensSiliconeSearch.html',
+    controller: 'productCtrl'
+  })
 
   $urlRouterProvider.otherwise('/home');
 
