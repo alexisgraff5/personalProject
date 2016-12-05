084e3530007068e69951a35efca0fc3d678c03a9
alexisgraff5 <lex.graff@gmail.com>
2016-12-05 11:39:33 -0700

    added banner images to a few pages

--- public/js/app.js
+++ public/js/app.js
@@ -5,47 +5,57 @@ angular.module('nixonClone', ['ui.router']).config(function($urlRouterProvider, 
     templateUrl: './views/home.html',
     controller: 'mainCtrl'
   })
-    .state('comp', {
+  .state('mens-watches', {
+    url: '/mens-watches',
+    templateUrl: './views/mens/mensWatches.html',
+    controller: 'productCtrl'
+  })
+  .state('womens-watches', {
+    url: '/womens-watches',
+    templateUrl: './views/womens/womensWatches.html',
+    controller: 'productCtrl'
+    })
+  .state('comp', {
      url: '/mens-comp',
      templateUrl: './views/mens/comp/compTemp.html',
      controller: 'productCtrl'
    })
-    .state('corporal', {
+  .state('corporal', {
     url: '/mens-corporal',
     templateUrl: './views/mens/corporal/corporalTemp.html',
     controller: 'productCtrl'
   })
-    .state('rollo', {
+  .state('rollo', {
     url: '/mens-rollo',
     templateUrl: './views/mens/rollo/rolloTemp.html',
     controller: 'productCtrl'
   })
-    .state('kenzi', {
+  .state('kenzi', {
     url: '/womens-kenzi',
     templateUrl: './views/womens/kenzi/kenziTemp.html',
     controller: 'productCtrl'
   })
-    .state('driver-leather', {
+  .state('driver-leather', {
     url: '/mens-driver-leather',
     templateUrl: './views/mens/driver/driverLeatherTemp.html',
     controller: 'productCtrl'
   })
-    .state('driver-ss', {
+  .state('driver-ss', {
     url: '/mens-driver-stainless',
     templateUrl: './views/mens/driver/driverSsTemp.html',
     controller: 'productCtrl'
   })
-    .state('gi-leather', {
+  .state('gi-leather', {
     url: '/womens-gi-leather',
     templateUrl: './views/womens/gi/giLeatherTemp.html',
     controller: 'productCtrl'
   })
-    .state('gi-nylon', {
+  .state('gi-nylon', {
     url: '/womens-gi-nylon',
     templateUrl: './views/womens/gi/giNylonTemp.html',
     controller: 'productCtrl'
   })
-    .state('gi-stainless', {
+  .state('gi-stainless', {
     url: '/womens-gi-stainless',
     templateUrl: './views/womens/gi/giSsTemp.html',
     controller: 'productCtrl'
